8eb2484fc44f2ced6edcbfd559aee8577e000a00
Ihor Yatsyshyn <74863323+Yatsa-dev@users.noreply.github.com>
2022-02-05 14:44:03 +0200

    Rostik`s tasks with transactions

---
+++ routes/usersRouter.js
@@ -0,0 +1,19 @@
+import {  validateUpdate, validateUsersQuery, validateId, validateUpdateBalance } from '../middlewares/validation/usersValidation';
+import roleAccess from '../middlewares/role-access';
+import userControllers from '../controllers/users/user-controllers';
+import guard from '../middlewares/guard'; 
+import {Router} from 'express';
+import { Role } from '../lib/constants';
+const router = new Router();
+
+router.get('/', [guard, roleAccess(Role.ADMIN), validateUsersQuery], userControllers.getUsers);
+
+router.get('/:id', [guard, validateId], userControllers.getUser);
+
+router.put('/:id/update', [guard, roleAccess(Role.ADMIN), validateId, validateUpdate], userControllers.putUser);
+
+router.delete('/:id', [guard, roleAccess(Role.ADMIN), validateId], userControllers.delUser);
+
+router.patch('/:id/balance', [guard, validateId, validateUpdateBalance], userControllers.putUser);
+
+export default router;
